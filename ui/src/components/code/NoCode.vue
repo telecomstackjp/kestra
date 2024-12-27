413fc216411e6645bca138d81b0f46dc48978199
MilosPaunovic <paun992@hotmail.com>
2024-12-27 08:59:23 +0100

    feat(ui): initial work on no code editor

---
+++ ui/src/components/code/NoCode.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Breadcrumbs :flow />
+
+        <hr class="m-0">
+
+        <Editor :metadata />
+    </div>
+</template>
+
+<script setup lang="ts">
+    import {computed} from "vue";
+
+    import YamlUtils from "../../utils/yamlUtils";
+
+    import Breadcrumbs from "./components/Breadcrumbs.vue";
+    import Editor from "./segments/Editor.vue";
+
+    const props = defineProps({flow: {type: Object, required: true}});
+
+    const metadata = computed(() => YamlUtils.getMetadata(props.flow.source));
+</script>
---
+++ ui/src/components/code/components/Breadcrumbs.vue
@@ -0,0 +1,30 @@
+<template>
+    <el-breadcrumb class="p-4">
+        <el-breadcrumb-item
+            v-for="(breadcrumb, index) in breadcrumbs"
+            :key="index"
+        >
+            <router-link :to="breadcrumb.to">
+                {{ breadcrumb.label }}
+            </router-link>
+        </el-breadcrumb-item>
+    </el-breadcrumb>
+</template>
+
+<script setup lang="ts">
+    import {computed} from "vue";
+
+    import {useI18n} from "vue-i18n";
+    const {t} = useI18n({useScope: "global"});
+
+    const props = defineProps({flow: {type: Object, required: true}});
+
+    const breadcrumbs = computed(() => {
+        return [
+            {
+                label: props.flow.id ?? t("create_flow"),
+                to: {},
+            },
+        ];
+    });
+</script>
