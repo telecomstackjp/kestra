b96248e7209e5e96017e598d2768b0e244b790d8
Bart Ledoux <bledoux@kestra.io>
2025-01-02 14:46:49 +0100

    update variables

--- ui/scripts/generate-palette.mjs
+++ ui/scripts/generate-palette.mjs
@@ -1,86 +1,82 @@
+// @ts-check
+
 import fs from "fs"
 import path from "path"
-import paletteLight from "./Sytem color-Light.json" with {type: "json"}
-import paletteDark from "./Sytem color-Dark.json" with {type: "json"}
+import figma from "./figma.json" with {type: "json"}
 
-
-function normalizeKey(key) {
-    return key.replace(/ /g, "-").toLowerCase()
-}
 const __dirname = path.dirname(new URL(import.meta.url).pathname)
+
+const [{
+    color:paletteLight
+}, {
+    color:paletteDark
+}] = figma[0].values
 
 makePalettes(paletteLight, "light", ":root")
 makePalettes(paletteDark, "dark", "html.dark")
 
-function makePalettes(palette, paletteName, selector) {
-    const baseColorNames = Object.keys(palette["base-color-palette"])
-    const colorIndex = {}
+function getValue(color, colorIndex) {
+    if (color.var.startsWith("base-color-palette/")) {
+        return `$base-${color.var.split("/").pop()}`
+    }
 
-    const scss = baseColorNames.map(colorName => {
-        const colorTheme = palette["base-color-palette"][colorName]
-        return Object.entries(colorTheme).map(([key, value]) => {
-            colorIndex[value] = normalizeKey(key)
-            return `$base-${normalizeKey(key)}: ${value};`
-        })
-        // sort the colors by length then by name
-        .sort((color1, color2) => {
-            const [color1Name] = color1.split(":")
-            const [color2Name] = color2.split(":")
-            return color1Name.length === color2Name.length ? color1Name.localeCompare(color2Name) : color1Name.length - color2Name.length
-        })
-        .join("\n")
-    }).join("\n")
+    // if the value of the color has 8 hexadecimal it has an alpha channel
+    // we therefore extract the color and compare it with the values in the base palette
+    if (color.value.length === 9) {
+        const alpha = Math.round(parseInt(color.value.slice(7), 16) / 2.55) / 100
+        const baseColor = color.value.slice(0, 7)
+        const baseColorName = colorIndex[baseColor]
+        if(baseColorName){
+            return `rgba($base-${baseColorName}, ${alpha})`
+        }
+    }
+    return color.value
+}
+
+/**
+ *
+ * @param {Array<{name:string, value:string, var:string }>} themeColors
+ * @returns
+ */
+function getVariableScss(themeColors, colorIndex) {
+    let prevCategory = null
+    const cssVariableNames = {}
+    const content = themeColors.map(c => {
+        const currentCategory = c.name.replace(/^ks\//, "").split("-")[0]
+        const val = getValue(c, colorIndex)
+        const categoryTitle = currentCategory !== prevCategory ? `\n\t/* ${currentCategory} */\n` : ""
+        cssVariableNames[currentCategory] = cssVariableNames[currentCategory] || []
+        const varName = c.name.replace(/\//g, "-")
+        cssVariableNames[currentCategory].push(varName)
+        prevCategory = currentCategory
+        return `${categoryTitle}\t#{--${varName}}: ${val};`
+    })
+
+    return {tokenScss: content.join("\n").trim(), cssVariableNames}
+}
+
+/**
+ *
+ * @param {Array<{name:string, value:string, var:string}>} palette
+ * @param {string} paletteName
+ * @param {string} selector
+ */
+function makePalettes(palette, paletteName, selector) {
+    const baseColorNames = palette.filter(c => c.name.startsWith("base-color-palette/"))
+    const colorIndex = baseColorNames.reduce((acc, color) => {
+        acc[color.value] = color.name.split("/").pop()
+        return acc
+    }, {})
+
+    const scss = baseColorNames.map(color => `$base-${color.name.split("/").pop()}: ${color.value}`).join("\n")
 
     // write the scss file containing colors in the base palette
     fs.writeFileSync(path.resolve(__dirname, "../src/styles/color-palette.scss"), scss, {encoding: "utf-8"})
 
-    const tokens = palette["ks"]
-
-    const cssVariableNames = {}
-
-    function getVariableScss(tokenTheme, tokenName, level = 0) {
-        // TODO: need to bring back the sections headers
-        const content = Object.entries(tokenTheme).map(([key, value]) => {
-            const normalizedKey = normalizeKey(key)
-            const prefix = tokenName ? `${tokenName}-${normalizedKey}` : normalizedKey
-
-            if(typeof value === "object") {
-                return getVariableScss(value, prefix, level + 1)
-            }
-
-            const tokenRoot = prefix.split("-")[1]
-            const cssVariableNamesTheme = cssVariableNames[tokenRoot] || []
-            cssVariableNamesTheme.push(prefix)
-            cssVariableNames[tokenRoot] = cssVariableNamesTheme
-
-            const colorVar = colorIndex[value] ? `$base-${colorIndex[value]}` : value
-            return `\t#{--${prefix}}: ${colorVar};`
-        }).sort()
-
-        if(level > 0) {
-            return content.join("\n")
-        }
-
-        // add categories comments to make it clearer to find
-        const contentWithCategoriesHeaders = []
-        let previousCategory = null
-        for (const line of content) {
-            const currentCategory = line.match(/#{--\w+-(\w+)/)?.[1] || "Other"
-            if (previousCategory !== currentCategory) {
-                contentWithCategoriesHeaders.push(`\n\t/* ${currentCategory} */`)
-            }
-            previousCategory = currentCategory
-
-            contentWithCategoriesHeaders.push(line)
-        }
-
-        return `${contentWithCategoriesHeaders.join("\n")}\n`
-    }
-
-    const tokenScss = `\t${getVariableScss(tokens, "ks").trim()}`
+    const {tokenScss, cssVariableNames} = getVariableScss(palette.filter(c => !c.name.startsWith("base-color-palette/")), colorIndex)
 
     // write the scss file containing colors in the token palette
-    fs.writeFileSync(path.resolve(__dirname, `../src/styles/layout/theme-${paletteName}.scss`), `@import "../color-palette.scss";\n\n${selector}{\n${tokenScss}\n}`, {encoding: "utf-8"})
+    fs.writeFileSync(path.resolve(__dirname, `../src/styles/layout/theme-${paletteName}.scss`), `@import "../color-palette.scss";\n\n${selector}{\n\t${tokenScss}\n}`, {encoding: "utf-8"})
 
     // write the css variables into an index for theme documentation
     // NOTE: we assume that all themes will have the same variables and write the same file over and over
