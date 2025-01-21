b604cc993d64a9a232f4f8adb021062d9a9d62a2
Bart Ledoux <bledoux@kestra.io>
2025-01-21 12:35:46 +0100

    add comments

--- ui/vite.config.js
+++ ui/vite.config.js
@@ -12,11 +12,13 @@ export default defineConfig({
         rollupOptions: {
             output: {
                 manualChunks: {
+                    // bundle dashboard and all its dependencies in a single chunk
                     "dashboard": [
                         "src/components/dashboard/Dashboard.vue", 
                         "src/components/dashboard/components/DashboardCreate.vue", 
                         "src/components/dashboard/components/DashboardEdit.vue"
                     ],
+                    // bundle flows and all its dependencies in a second chunk
                     "flows": [
                         "src/components/flows/Flows.vue", 
                         "src/components/flows/FlowCreate.vue", 
