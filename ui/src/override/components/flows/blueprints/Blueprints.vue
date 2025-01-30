4d04795b13fcda664fa76e4d01b8f661c2d3341e
Bart Ledoux <bledoux@kestra.io>
2025-01-30 12:58:09 +0100

    feat: add locks back on tabs

--- ui/src/override/components/flows/blueprints/Blueprints.vue
+++ ui/src/override/components/flows/blueprints/Blueprints.vue
@@ -104,7 +104,8 @@
                     {
                         name: "custom",
                         title: this.$t("blueprints.custom"),
-                        query: this.$route.query
+                        query: this.$route.query,
+                        locked: true
                     }
                 ]
             },
