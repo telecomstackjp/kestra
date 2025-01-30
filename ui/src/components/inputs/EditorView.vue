a7f2e36af7c31d58f48f8a633b36f184c10012e6
MilosPaunovic <paun992@hotmail.com>
2025-01-30 09:40:22 +0100

    fix(ui): allow creation of multiple tasks from the no code editor

--- ui/src/components/inputs/EditorView.vue
+++ ui/src/components/inputs/EditorView.vue
@@ -799,7 +799,7 @@
         }
 
         haveChange.value = true;
-        store.dispatch("core/isUnsaved", true);
+        if(editorViewType.value === "YAML") store.dispatch("core/isUnsaved", true);
 
         if(!props.isCreating){
             store.commit("editor/changeOpenedTabs", {
